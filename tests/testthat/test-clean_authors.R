23698a6de14d350454700ba1544bdeb57bc5b7df
Ildiko Czeller <czeildi@gmail.com>
2019-09-19 15:23:55 +0100

    :white_check_mark: add skeleton for tests and 1 test

--- tests/testthat/test-clean_authors.R
+++ tests/testthat/test-clean_authors.R
@@ -1,3 +1,36 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+context("clean_authors")
+
+describe("unify_names_for_login", {
+  commits <- tibble::tribble(
+    ~repo,~author_login,~commit_author_name,
+    "r1","a1","name1",
+    "r2","a1","name2",
+    "r3","a1","name1",
+    "r4","a2","name2"
+  )
+  it("selects most frequent names", {
+    expect_equal(
+      unify_names_for_login(commits),
+      tibble::tribble(
+        ~repo,~author_login,~commit_author_name,
+        "r1","a1","name1",
+        "r2","a1","name1",
+        "r3","a1","name1",
+        "r4","a2","name2"
+      )
+    )
+  })
 })
+
+describe("identify authors", {
+  describe("by_name", {
+
+  })
+  describe("by_email", {
+
+  })
+  describe("by_login_as_name", {
+
+  })
+})
+
